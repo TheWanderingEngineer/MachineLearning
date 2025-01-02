3bf80545cf6d0f478f6a3e2752beef0067ac89d4
Moy <156215775+TheWanderingEngineer@users.noreply.github.com>
2025-01-02 12:55:04 +0300

    learning curves and others

--- training_utils.py
+++ training_utils.py
@@ -92,6 +92,10 @@ class ModelManager():
                     self.epoch_stats["test_acc"].append(test_acc_epoch)
         end_time = time()
         self.train_test_time = end_time-start_time
+        if self.test_dl:
+            print(f"Training and Testing completed in {self.train_test_time:.2f} seconds.")
+        else:
+            print(f"Training completed in {self.train_test_time:.2f} seconds.")
     
     def evaluate(self):
         pass
@@ -99,18 +103,42 @@ class ModelManager():
     def predict(self):
         pass
 
-    def learning_curves(self):
-        pass
+    def learning_curves(self, acc = False):
+        fig, (ax1,ax2) = plt.subplots(1,2,figsize=(15,10))
+        fig.suptitle(f"Learning Curves of Model: {self.model}", fontsize=16)
+        steps = range(len(self.batch_stats["train_loss"]))
+        ax1.plot(steps, self.batch_stats["train_loss"], color = 'blue', label = "Train Loss")
+        if len(self.batch_stats["test_loss"]) > 0:
+            ax1.plot(steps, self.batch_stats["test_loss"], color = 'green', label = "Test Loss")
+        else:
+            print("Warning: Test Loss is empty. Skipping test loss curve.")
+        ax1.set_title("Loss Curves")
+        ax1.set_xlabel("Steps (In Batches)")
+        ax1.set_ylabel("Loss")
+        ax1.legend()
+        if acc:
+            ax2.plot(steps, self.batch_stats["train_acc"], color = 'blue', label = "Train Accuracy")
+            if len(self.batch_stats["test_acc"]) > 0:
+                ax2.plot(steps, self.batch_stats["test_acc"], color = 'green', label = "Test Accuracy")
+            else:
+                print("Warning: Test Accuracy is empty. Skipping test accuracy curve.")
+            ax2.set_title("Accuracy Curves")
+            ax2.set_xlabel("Steps (In Batches)")
+            ax2.set_ylabel("Accuracy")
+            ax2.legend()
+        plt.tight_layout()
+        plt.show()
 
     def summary(self, hyperparamters = False):
         print("____________________________ Train Summary ____________________________")
         print(f"Final Training Loss: {self.epoch_stats['train_loss'][-1]}")
-        print(f"Final Training Accuracy: {self.epoch_stats['train_acc'][-1]*100}%")
+        print(f"Final Training Accuracy: {(self.epoch_stats['train_acc'][-1]*100):.2f}%")
         if self.train_dl:
             print("____________________________ Test Summary ____________________________")
             print(f"Final Testing Loss: {self.epoch_stats['test_loss'][-1]}")
             print(f"Final Testing Accuracy: {self.epoch_stats['test_acc'][-1]*100}%")
-            print(f"Best Testing Accuracy: {max(self.epoch_stats['test_acc']):.2f}% at epoch {np.argmax(self.epoch_stats['test_acc'])+1}")
+
+            print(f"Best Testing Accuracy: {max(self.epoch_stats['test_acc'])*100:.2f}% at epoch {np.argmax(self.epoch_stats['test_acc'])+1}")
             print(f"\nTraining & Testing time: {self.train_test_time:.2f} seconds")
         else:
             print(f"\nTraining time: {self.train_test_time:.2f} seconds")
